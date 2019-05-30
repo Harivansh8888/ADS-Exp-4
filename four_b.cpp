244551d8d1fb21aa28511c1fee5f5ca14225e3e9
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-05-30 11:17:37 +0530

    Added class Hydrogen

--- four_b.cpp
+++ four_b.cpp
@@ -11,4 +11,18 @@ class oxygen
   public:
    friend void compound(oxygen&a1,hydrogen&b1,sulphur&c1);
     oxygen(){  cout<<"Enter molecular mass of oxygen:";  cin>>o;  }
-}; 
+};
+
+
+
+class hydrogen
+{
+  private :
+              int h;
+
+  public:
+    friend void compound(oxygen&a1,hydrogen&b1,sulphur&c1);
+      hydrogen(){  cout<<"Enter molecular mass of hydrogen";   cin>>h;  }
+
+};
+
