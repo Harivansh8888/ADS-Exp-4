6a45c3a257d0455d43ed82729edb152078f6d30a
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-05-30 11:18:31 +0530

    Added class sulphur

--- four_b.cpp
+++ four_b.cpp
@@ -25,4 +25,12 @@ class hydrogen
       hydrogen(){  cout<<"Enter molecular mass of hydrogen";   cin>>h;  }
 
 };
+class sulphur
+{
+    public:
+    int s;
+   public :
+     friend void compund(oxygen&a1,hydrogen&b1,sulphur&c1);
+               sulphur() {  cout<<"Enter molecular mass of sulphur";   cin>>s;  }
 
+}; 
