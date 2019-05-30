686146f5cc302ba629820e3a11107513002f98aa
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-05-30 11:13:20 +0530

    Added class Internal Exam

--- four_a.cpp
+++ four_a.cpp
@@ -22,3 +22,36 @@ class Student
 			cout<<"Gender: "<<gender<<endl;
 		}
 };
+class Internal_Exam:public virtual Student
+{
+	public:
+		int internal_eng,internal_math,internal_phys,internal_chem,internal_ped,internal_comp;
+		get_internal_marks()
+		{
+			cout<<"//ENTER ALL MARKS OUT OF 100//"<<endl;
+			cout<<endl;
+			cout<<"Enter Internal Marks for English:"<<endl;
+			cin>>internal_eng;
+			cout<<"Enter Internal Marks for Mathematics:"<<endl;
+			cin>>internal_math;
+			cout<<"Enter Internal Marks for Physics:"<<endl;
+			cin>>internal_phys;
+			cout<<"Enter Internal Marks for Chemistry:"<<endl;
+			cin>>internal_chem;
+			cout<<"Enter Internal Marks for Physical Education:"<<endl;
+			cin>>internal_ped;
+			cout<<"Enter Internal Marks for Computers:"<<endl;
+			cin>>internal_comp;
+		}
+		display_internal_marks()
+		{
+			cout<<"Internal Marks are:"<<endl;
+			cout<<"English: "<<internal_eng<<endl;
+			cout<<"Mathematics: "<<internal_math<<endl;
+			cout<<"Physics: "<<internal_phys<<endl;
+			cout<<"Chemistry: "<<internal_chem<<endl;
+			cout<<"Physical Education: "<<internal_ped<<endl;
+			cout<<"Computers: "<<internal_comp<<endl;
+		}
+
+};
